d5adf8476a56bd7245897ec9409359787826fe32
PraneetMane2208 <praneetmane2208@gmail.com>
2023-02-11 03:41:31 +0530

    Refreshing token 2.getting current user

--- soul/settings.py
+++ soul/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,14 +151,18 @@ REST_FRAMEWORK={
     # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination'
 }
 
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
+
 
 AUTH_USER_MODEL='core.User'
 
 DJOSER={
     'SERIALIZERS':{
-        'user_create':'core.serializers.UserCreateSerializer'
+        'user_create':'core.serializers.UserCreateSerializer',
+        'current_user':'core.serializers.UserSerializer'
     }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
